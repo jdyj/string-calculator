5790eec89b240ca8b3e72da9e3277596f896062a
jdyj <jdyj@naver.com>
2022-02-12 13:49:51 +0900

    [ADD] 큰 수 덧셈, 뺄셈 구현

--- app/src/main/java/string/calculator/Calculate.java
+++ app/src/main/java/string/calculator/Calculate.java
@@ -7,17 +7,17 @@ import java.util.Stack;
 public class Calculate {
 
   private final Stack<Character> operatorStack = new Stack<>();
-  private final Stack<Double> numberStack = new Stack<>();
+  private final Stack<String> numberStack = new Stack<>();
   private final StringBuilder sb = new StringBuilder();
   private final char[] operators = {'+', '-', '(', ')', '/', '*'};
 
   public void addNumber(char c) {
-    if (c >= '0' && c <= '9') {
+    if ((c >= '0' && c <= '9') || c == '-') {
       sb.append(c);
     }
   }
 
-  public double run(String input) {
+  public String run(String input) {
 
     List<Character> chars = input.chars()
         .mapToObj(c -> (char) c)
@@ -42,7 +42,7 @@ public class Calculate {
   public void ifLastCharNumber(char c) {
     if (c >= '0' && c <= '9') {
       sb.append(c);
-      numberStack.add(Double.parseDouble(sb.toString()));
+      numberStack.add(sb.toString());
       checkLastStack();
     }
   }
@@ -57,7 +57,7 @@ public class Calculate {
 
   public void stackAdd(Character sign) {
     if (!sb.isEmpty()) {
-      numberStack.add(Double.parseDouble(sb.toString()));
+      numberStack.add(sb.toString());
     }
     if (sign.equals('+')) {
       if (!operatorStack.isEmpty()) {
@@ -75,7 +75,7 @@ public class Calculate {
           calculateOne();
         }
       }
-      operatorStack.add(sign);
+      operatorStack.add('+');
     }
 
     if (sign.equals('*')) {
@@ -118,8 +118,8 @@ public class Calculate {
 
   public void calculateOne() {
 
-    Double rightValue = numberStack.pop();
-    Double leftValue = numberStack.pop();
+    String rightValue = numberStack.pop();
+    String leftValue = numberStack.pop();
 
     Character operator = operatorStack.pop();
     operatorSelect(leftValue, rightValue, operator);
@@ -137,17 +137,13 @@ public class Calculate {
 
   }
 
-  public void operatorSelect(Double leftValue,
-      Double rightValue,
+  public void operatorSelect(String leftValue,
+      String rightValue,
       Character operator) {
 
-    double result;
+    String result;
     if (operator.equals('+')) {
       result = add(leftValue, rightValue);
-      numberStack.push(result);
-    }
-    if (operator.equals('-')) {
-      result = subtract(leftValue, rightValue);
       numberStack.push(result);
     }
     if (operator.equals('*')) {
@@ -160,17 +156,14 @@ public class Calculate {
     }
   }
 
-  public double getResult() {
-
-    reverseStack(operatorStack);
-    reverseStack(numberStack);
+  public String getResult() {
 
     while (!numberStack.isEmpty()) {
       if (numberStack.size() == 1) {
         break;
       }
-      Double leftValue = numberStack.pop();
-      Double rightValue = numberStack.pop();
+      String leftValue = numberStack.pop();
+      String rightValue = numberStack.pop();
 
       Character operator = operatorStack.pop();
       operatorSelect(leftValue, rightValue, operator);
@@ -179,34 +172,141 @@ public class Calculate {
     return numberStack.pop();
   }
 
-  private <T> void reverseStack(Stack<T> stack) {
+  private String add(String leftValue, String rightValue) {
 
-    List<T> tempList = new ArrayList<>(stack);
-    stack.clear();
+    int leftSize = leftValue.length();
+    int rightSize = rightValue.length();
 
-    for (int i = tempList.size() - 1; i >= 0; i--) {
-      stack.push(tempList.get(i));
+    int size = Math.max(leftSize, rightSize);
+
+    int[] left = new int[size + 1];
+    int[] right = new int[size + 1];
+
+    // true 음수, false 양수
+    boolean leftSign = leftValue.charAt(0) == '-';
+    boolean rightSign = rightValue.charAt(0) == '-';
+
+    reverseInput(leftValue, leftSize, left);
+    reverseInput(rightValue, rightSize, right);
+
+    boolean minus = false;
+
+    // 부호가 같을 때
+    if (leftSign == rightSign) {
+      for (int i = 0; i < size; i++) {
+        int temp = left[i] + right[i];
+        left[i] = temp % 10;
+        left[i + 1] += temp / 10;
+      }
+
+      if (leftSign) {
+        minus = true;
+      }
+    }
+
+    // 부호가 다를 때
+    if (leftSign != rightSign) {
+      //true left, false right
+      boolean sign = signDecision(size, left, right);
+
+      if (sign) {
+        subtract(size, left, right);
+        if (leftSign) {
+          minus = true;
+        }
+      }
+
+      if (!sign) {
+        subtract(size, right, left);
+        left = right;
+        if (rightSign) {
+          minus = true;
+        }
+      }
+
+    }
+
+    StringBuilder stringBuilder = new StringBuilder();
+
+    if (left[size] != 0) {
+      stringBuilder.append(left[size + 1]);
+    }
+
+    for (int i = size - 1; i >= 0; i--) {
+      if (i == size - 1 && minus) {
+        stringBuilder.append('-');
+        continue;
+      }
+      stringBuilder.append(left[i]);
+    }
+
+    checkZero(stringBuilder);
+    return stringBuilder.toString();
+  }
+
+  private void checkZero(StringBuilder stringBuilder) {
+
+    boolean isValid = false;
+    while (!isValid) {
+      if (stringBuilder.charAt(0) == '0') {
+        if (stringBuilder.length() == 1) {
+          break;
+        }
+        stringBuilder.deleteCharAt(0);
+      }
+      if (stringBuilder.charAt(0) > '0' && stringBuilder.charAt(0) <= '9') {
+        isValid = true;
+      }
+      if (stringBuilder.charAt(0) == '-') {
+        isValid = true;
+      }
     }
 
   }
 
-  private double add(double leftValue, double rightValue) {
-    return leftValue + rightValue;
-  }
-
-  private double subtract(double leftValue, double rightValue) {
-    return leftValue - rightValue;
-  }
-
-  private double multiply(double leftValue, double rightValue) {
-    return leftValue * rightValue;
-  }
-
-  private double divide(double leftValue, double rightValue) {
-    if (rightValue == 0) {
-      throw new IllegalStateException("0으로 나눌 수 없습니다");
+  private void reverseInput(String value, int size, int[] array) {
+    for (int i = size - 1; i >= 0; i--) {
+      if (value.charAt(i) == '-') {
+        continue;
+      }
+      array[size - i - 1] = value.charAt(i) - '0';
     }
-    return leftValue / rightValue;
+  }
+
+  private void subtract(int size, int[] left, int[] right) {
+    for (int i = 0; i < size; i++) {
+      int temp = left[i] - right[i];
+      if (temp < 0) {
+        temp += 10;
+        left[i + 1]--;
+      }
+      left[i] = temp;
+    }
+  }
+
+  private boolean signDecision(int size, int[] left, int[] right) {
+    for (int i = size - 1; i >= 0; i--) {
+      if (left[i] > right[i]) {
+        return true;
+      }
+      if (left[i] < right[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private String multiply(String leftValue, String rightValue) {
+//    return leftValue * rightValue;
+    return null;
+  }
+
+  private String divide(String leftValue, String rightValue) {
+//    if (rightValue == 0) {
+//      throw new IllegalStateException("0으로 나눌 수 없습니다");
+//    }
+//    return leftValue / rightValue;
+    return null;
   }
 
 }
--- app/src/test/java/string/calculator/CalculateTests.java
+++ app/src/test/java/string/calculator/CalculateTests.java
@@ -9,25 +9,35 @@ class CalculateTests {
 
   private final Calculate calculate = new Calculate();
 
-  @Test
-  @DisplayName("우선순위(괄호)가 들어간 계산")
-  void runTest() {
-    String input = "5 * (1+3/3*4+(5-6)*7) - 5";
-    assertEquals(calculate.run(input), -15);
-  }
+//  @Test
+//  @DisplayName("우선순위(괄호)가 들어간 계산")
+//  void runTest() {
+//    String input = "5 * (1+3/3*4+(5-6)*7) - 5";
+//    assertEquals(calculate.run(input), "-15");
+//  }
 
-  @Test
-  @DisplayName("divide 0")
-  void divide0Test() {
-    String input = "5 / 0";
-    assertThrows(IllegalStateException.class, () -> calculate.run(input));
-  }
+//  @Test
+//  @DisplayName("divide 0")
+//  void divide0Test() {
+//    String input = "5 / 0";
+//    assertThrows(IllegalStateException.class, () -> calculate.run(input));
+//  }
 
   @Test
   @DisplayName("중첩 괄호 4개")
   void runTest2() {
     String input = "((((1+3))))";
-    assertEquals(calculate.run(input), 4);
+    assertEquals(calculate.run(input), "4");
+  }
+
+  @Test
+  @DisplayName("큰수 계산")
+  void runTest3() {
+    String input =
+        "13212512461376188834613478867543213456786543245678654365746347585746357687757654324321234545678976543567765434"
+            + "+215879654325678973456787687653467865433676854654356786543567866543245678765432173";
+    assertEquals(calculate.run(input),
+        "13212512461376188834613478867759093111112222219135442053399815451180034542412011110864802412222222222333197607");
   }
 
 }
