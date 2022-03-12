11648bbf832884ed8bdb066fade740ac94c8e851
jdyj <jdyj@naver.com>
2022-03-12 16:48:06 +0900

    [FIX] Code 리팩토링 - 추상화 레벨 맞추기

--- app/src/main/java/com/string/calculator/Number.java
+++ app/src/main/java/com/string/calculator/Number.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * 얘는 딱 인풋을 받으면 숫자들은 숫자 스택에, 연산자는 연산자스택에 넣어주는 역할만 하고싶은데 내가 설계한 계산기 특성상 높은 우선순위의 연산자가 있으면 연산을 해줘야 하는
- * 상황....
+ * 얘는 딱 인풋을 받으면 숫자들은 숫자 스택에, 연산자는 연산자스택에 넣어주는 역할만 하고싶은데 <-- 이것도 책임이 많은편인건가
+ * 내가 설계한 계산기 특성상 높은 우선순위의 연산자가 있으면 연산을 해줘야 하는 상황....
  */
 public class Number {
 
@@ -26,9 +26,9 @@ public class Number {
 
     for (Character c : chars) {
       ifExistHighOperatorCalculate();
-      addOperatorSignToStack(c);
 
-      ifBlankCheckNumberInStringBuilder(c);
+      addOperatorSignToStack(c);
+      addNumberToStack(c);
       addNumberToStringBuilder(c);
     }
 
@@ -71,7 +71,7 @@ public class Number {
   }
 
   public void addOperatorSignToStack(Character c) {
-    if (OperatorSign.findOperator(c)) {
+    if (OperatorSign.isSupportedOperator(c)) {
       operatorSignStack.add(OperatorSign.valueOf(c));
     }
   }
@@ -83,24 +83,30 @@ public class Number {
   }
 
 
+  /**
+   * 한가지 동작은 기준에 따라 다르다.
+   * 보통 그 기준은 추상화 레벨에 따라 달라진다.
+   */
+  // if 3개를 하나의 의미로 합치면 높은 우선순위의 연산자가 있는지 확인하는 절차
+  // 메서드 이름과 실제 메서드 동작이 일치하지 않는다.
+  // calculate 메서드안에 calculateOne이 또 나온다
+  // high operator에 대한 의미가 숨겨져있다. (타입이나 메서드 이름 등으로 나왔으면 좋겠다.)
   private void ifExistHighOperatorCalculate() {
     if (operatorSignStack.isEmpty()) {
       return;
     }
-
-    if (operatorSignStack.size() >= numberStack.size()) {
+    OperatorSign lastOperator = operatorSignStack.peek();
+    if (!(lastOperator == OperatorSign.divide || lastOperator == OperatorSign.multiply)) {
       return;
     }
 
-    OperatorSign lastOperator = operatorSignStack.peek();
-    if (lastOperator == OperatorSign.divide || lastOperator == OperatorSign.multiply) {
-      String leftValue = getNumber();
-      String rightValue = getNumber();
-      OperatorSign operatorSign = getOperatorSign();
-      String result = operator.calculateOne(leftValue, rightValue, operatorSign);
-      numberStack.add(result);
-    }
+    String leftValue = getNumber();
+    String rightValue = getNumber();
+    OperatorSign operatorSign = getOperatorSign();
+    String result = operator.calculateOne(leftValue, rightValue, operatorSign);
 
+
+    numberStack.add(result);
   }
 
   private void addNumberToStackWithStringBuilder() {
@@ -110,16 +116,30 @@ public class Number {
     }
   }
 
-  private void ifBlankCheckNumberInStringBuilder(char c) {
-    if (c != ' ') {
-      return;
-    }
-
-    if (stringBuilder.length() > 0) {
+  /**
+   * 빈칸인지 체크, 스트링빌더에 숫자가 있는지 확인후 스택에 추가
+   * 메서드 이름 ifBlankCheckNumberInStringBuilder를 보았을때 빈칸이면 스트링빌더에 숫자를 체크한다는 것으로만 보임. 수정 필요.
+   * 결국 이 메서드가 하는 동작은 스트링빌더에 있는 숫자를 스택에 넣으려고 한 것이니, 스택에 넣을수 있을지 없을지만 확인하면 되는걸로 보임.
+   * 그러면 원래 빈칸인지?, 숫자가 있는지? 묻던 걸 하나로 묶어 추상화 레벨을 맞춰주면 될거 같음.
+   * 근데 어떻게 묶어볼까 --> private 메서드
+   * 이유는? --> 빈칸인지 확인하는 for 문 부분도 해당 클래스에 있고 숫자를 저장해두는 StringBuilder도 해당 클래스의 필드 부분에 있으므로
+   * 응집성이 높아질 것이라고 판단.
+   *
+   */
+  private void addNumberToStack(char c) {
+    if (canAddNumberToStack(c)) {
       numberStack.add(stringBuilder.toString());
+      stringBuilder.setLength(0);
     }
+  }
 
-    stringBuilder.setLength(0);
+  private boolean canAddNumberToStack(char c) {
+    // 빈칸인지?
+    if (c == ' ') {
+      return true;
+    }
+    // 숫자가 있는지
+    return stringBuilder.length() > 0;
   }
 
   private String calculateLeftover() {
