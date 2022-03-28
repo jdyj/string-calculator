6ef5bda331ea55c85010f3c4e58e10c603fe41c2
jdyj <jdyj@naver.com>
2022-03-28 20:26:15 +0900

    [refactor] : 테스트 자동화

--- app/src/test/java/com/string/calculator/AppTest.java
+++ app/src/test/java/com/string/calculator/AppTest.java
@@ -3,24 +3,32 @@
  */
 package com.string.calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
 
   @Test
-  @DisplayName("main 테스트")
-  void main() {
+  @DisplayName("main 테스트 - 사칙 연산 테스트")
+  void main1() {
     String[] args = null;
     String input =
         "32435456436754325674356756762221212798 - 342283024803781287013 * 235465789800876543223456543454 + 12341251246 - 3453476564534 + 13476857565743 * 1234567";
     InputStream in = new ByteArrayInputStream(input.getBytes());
+    OutputStream out = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(out);
     System.setIn(in);
+    System.setOut(printStream);
     App.main(args);
-//    -80595942770822941182435736008003954206997027825111
-//    -80595942770822941182435736008003954206997027825111
+
+    assertEquals("num : -80595942770822941182435736008003954206997027825111\n", out.toString());
   }
 
 }
