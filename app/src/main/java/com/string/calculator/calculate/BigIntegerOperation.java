1c8069b0efb5ae9a1df7ea042fda1fd0265462e1
jdyj <jdyj@naver.com>
2022-06-22 03:41:07 +0900

    [refactor] : BigInteger 변경 후 나머지 연산 추가

---
+++ app/src/main/java/com/string/calculator/calculate/BigIntegerOperation.java
@@ -0,0 +1,43 @@
+package com.string.calculator.calculate;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class BigIntegerOperation implements ArithmeticOperation {
+
+  private final BigInteger left;
+  private final BigInteger right;
+
+  @Override
+  public String plus() {
+    return left.add(right).toString();
+  }
+
+  @Override
+  public String subtract() {
+    return left.subtract(right).toString();
+  }
+
+  @Override
+  public String multiply() {
+    return left.multiply(right).toString();
+  }
+
+  @Override
+  public String divide() {
+    return left.divide(right).toString();
+  }
+
+  @Override
+  public String modular() {
+    if (left.compareTo(right) < 0) {
+      return right.mod(left).toString();
+    }
+    return left.mod(right).toString();
+  }
+
+  public BigIntegerOperation(String leftValue, String rightValue) {
+    left = new BigInteger(leftValue);
+    right = new BigInteger(rightValue);
+  }
+}
