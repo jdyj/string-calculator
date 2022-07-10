eb65a07c33fd32466d8da264b231b7057e239209
jdyj <jdyj@naver.com>
2022-07-10 17:31:57 +0900

    [add] : 분수 표현 기능 추가

--- app/src/main/java/com/string/calculator/output/PlainOutput.java
+++ app/src/main/java/com/string/calculator/output/PlainOutput.java
@@ -10,7 +10,49 @@ public class PlainOutput implements Output {
 
   @Override
   public void print() {
+    if (isFraction()) {
+      StringBuilder sb = new StringBuilder();
+      String[] split = result.split("/");
+      int numeratorLength = split[0].length();
+      int denominatorLength = split[1].length();
+      int barCount = Math.max(numeratorLength, denominatorLength);
+      if (isMinus()) {
+        split[0] = "  " + split[0].substring(1);
+        numeratorLength -= 1;
+        split[1] = "  " + split[1];
+      }
+      if (numeratorLength < denominatorLength) {
+        int space = (barCount - numeratorLength) / 2;
+        for (int i = 0; i < space; i++) {
+          split[0] = " " + split[0];
+        }
+      } else if (numeratorLength > denominatorLength) {
+        int space = (barCount - split[1].length()) / 2;
+        for (int i = 0; i < space; i++) {
+          split[1] = " " + split[1];
+        }
+      }
+      sb.append(split[0]);
+      sb.append("\n");
+      if (isMinus()) {
+        sb.append("- ");
+      }
+      sb.append("-".repeat(barCount));
+      sb.append("\n");
+      sb.append(split[1]);
+      System.out.println(sb);
+      return;
+    }
+
     System.out.println("num : " + result);
+  }
+
+  private boolean isFraction() {
+    return result.contains("/");
+  }
+
+  private boolean isMinus() {
+    return result.contains("-");
   }
 
 }
