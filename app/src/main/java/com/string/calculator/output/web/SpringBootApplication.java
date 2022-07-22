1d489b185a17ce844a714fa8bf6f179f04fe4988
jdyj <jdyj@naver.com>
2022-07-22 16:58:31 +0900

    [add] : web server 포맷 별 content-type 맞추도록 수정

--- app/src/main/java/com/string/calculator/output/web/SpringBootApplication.java
+++ app/src/main/java/com/string/calculator/output/web/SpringBootApplication.java
@@ -8,6 +8,7 @@ public class SpringBootApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringBootApplication.class, args);
+    System.out.println("http://localhost:8080/result");
   }
 
 
